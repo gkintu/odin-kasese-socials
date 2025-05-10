12aefcd39c010097f5091c70baa4c7cc2e77ab4c
George Kintu <195527580+gkintu@users.noreply.github.com>
2025-05-10 06:43:06 +0200

    Add ESLint and Prettier configuration, set up Husky pre-commit hooks, and update package.json scripts

---
+++ prettier.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  semi: true, // Add semicolons at the end of statements.
+  singleQuote: true, // Use single quotes instead of double quotes for strings.
+  trailingComma: 'es5', // Add trailing commas where valid in ES5 (objects, arrays, etc.).
+  tabWidth: 2, // Indent lines with 2 spaces.
+  printWidth: 80, // Wrap lines that exceed 80 characters.
+};
